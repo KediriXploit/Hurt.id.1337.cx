bd7227e3cd24d9642f2350445fcb5dbd95c6625b
KediriXploit <121042520+KediriXploit@users.noreply.github.com>
2023-02-24 23:15:41 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,59 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Defacer Hurt.id</title>
+
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="title" content="Defacer Hurt">
+<meta name="description" content="AgressivTeam">
+<meta name="author" content="Defacer Hurt">
+<meta name="copyright" content="Defacer Hurt.id">
+<meta name="keyword" content="Defacer Hurt.id">
+<meta name="rating" content="general">
+<meta name="googlebot" content="all, index, follow">
+<meta name="robots" content="index, follow">
+<meta name="googlebot-news" content="all">
+<meta name="title" content="Defacer Hurt.id">
+<meta property="description" content="XRelaX Sec Team">
+<meta property="og:image" content="https://sman5cirebon.sch.id/files/rot.gif">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="Atribut/all.min.css">
+<link rel="shortcut icon" href="https://files.civicsun.com/Hurt.png">
+
+</head>
+
+<body style="color: #fff; margin:0;font: normal 14px/20px Share Tech Mono, Helvetica, sans-serif; height:100%; background-color: #000000;"/>
+
+<div style="height:auto; min-height:100%; ">
+<div style="text-align: center; width:800px; margin-left: -400px; position:absolute; top: 20%; left:50%;">
+
+<!-- gambar -->
+<img style="width:400px;" src="https://sman5cirebon.sch.id/files/rot.gif"></img>
+
+<p></p>
+<font size="6" color="white" class="text-center text-red mb-6">Defacer Hurt.id
+</font>
+<br></br>
+<marquee behavior="alternate" scrollamount="5" style="border:0px solid;" width="60%"> <font color="white-red" face="ubold">
+ [[ Ternyata kehilangan itu menyakitkan, tetapi ada yg lebih sakit yaitu tidak pernah dianggap ada ]]
+
+ </font>
+</marquee>
+<p></p>
+<center>
+
+<audio autoplay="autoplay" controls="controls" src="https://simak.stt.web.id/uploads/v.mp3" type="audio/mpeg"/>
+
+</center>
+
+<p></p>
+<font size="4" color="white" class="text-center text-black mb-5">Contact me: Defacer.Hurt.id@protonmail.com
+</font>
+
+<script>alert(" ~Defacer Hurt.id~ ");</script>
+
+</body>
+</html>
